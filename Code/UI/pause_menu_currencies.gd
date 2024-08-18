09534a1d0d453b9b09390c8caea5ed1254ae23d6
Symbol24 <phillip.conte@gmail.com>
2024-08-17 23:45:25 -0400

    keys added to inventory ui

--- Code/UI/pause_menu_currencies.gd
+++ Code/UI/pause_menu_currencies.gd
@@ -1,6 +1,9 @@
 extends RControl
 
 @onready var gold:RRichTextLabel = %gold
+@onready var key_1: RRichTextLabel = %key1
+@onready var key_2: RRichTextLabel = %key2
+@onready var key_3: RRichTextLabel = %key3
 
 func _ready():
 	super()
@@ -12,3 +15,15 @@ func _update_all_currencies():
 			gold.text = str(PlayerData.data.currencies[str(GM.CURRENCIES.GOLD)])
 		else:
 			gold.text = "0"
+		if PlayerData.data.currencies.has(str(GM.CURRENCIES.KEY1)):
+			key_1.text = str(PlayerData.data.currencies[str(GM.CURRENCIES.KEY1)])
+		else:
+			key_1.text = "0"
+		if PlayerData.data.currencies.has(str(GM.CURRENCIES.KEY2)):
+			key_2.text = str(PlayerData.data.currencies[str(GM.CURRENCIES.KEY2)])
+		else:
+			key_2.text = "0"
+		if PlayerData.data.currencies.has(str(GM.CURRENCIES.KEY3)):
+			key_3.text = str(PlayerData.data.currencies[str(GM.CURRENCIES.KEY3)])
+		else:
+			key_3.text = "0"
